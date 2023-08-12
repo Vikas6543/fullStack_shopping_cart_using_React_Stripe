ac50a5fe0720c71589fdadb432529a4d4b5198df
vikase9bit <you@example.com>
2023-08-12 10:47:26 +0530

    deploy config

--- server.js
+++ server.js
@@ -42,10 +42,10 @@ app.use('/payment', require('./routes/paymentRoute'));
 
 // configuarations for deployment to vercel
 if (process.env.NODE_ENV === 'production') {
-  app.get('/', (req, res) => {
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'client/build/index.html'))
+  );
 }
 
 const PORT = process.env.PORT || 5000;
